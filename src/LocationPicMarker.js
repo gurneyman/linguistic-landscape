d9a0b7fab92cd05c5fae407ab89b87e040deb046
Tom Prince <tprince14@gmail.com>
2017-10-30 19:23:00 -0400

    begin moving pic state up the app

--- src/LocationPicMarker.js
+++ src/LocationPicMarker.js
@@ -1,38 +1,16 @@
 import React, { Component } from 'react';
 import {
-  Marker,
-  InfoWindow,
+  Marker
 } from "react-google-maps";
 
 class LocationPicMarker extends Component {
-  state = {
-    isOpen: false,
-  };
-
-  toggleOpen = () => {
-      this.setState({
-          isOpen: !this.state.isOpen,
-      })
-  }
-
   render() {
     const locationPic = this.props.locationPic
     return (
       <div>
         <Marker
             position={ locationPic.location }
-            onClick={ this.toggleOpen }>
-            {this.state.isOpen &&
-                <InfoWindow
-                    onCloseClick={ this.toggleOpen }>
-                    <div>
-                        <h2>{ locationPic.title }</h2>
-                        <img 
-                            src={ locationPic.imgurl }
-                            alt="rawr" />
-                    </div>
-                </InfoWindow>
-            }
+            onClick={ () => this.props.toggleOpenAt() }>
         </Marker>
       </div>
     );
--- src/MarkerList.js
+++ src/MarkerList.js
@@ -17,6 +17,7 @@ class MarkerList extends Component {
         imgurl: "https://farm5.staticflickr.com/4450/37396723076_8df528500a_z_d.jpg",
         address: "Kung Fu Tea, 234 Canal St, New York, NY 10013",
         location: new Location(40.7176948, -74.0000486),
+        isActive: true,
       },
       {
         id: 2,
@@ -24,6 +25,7 @@ class MarkerList extends Component {
         imgurl: "https://farm5.staticflickr.com/4383/37396722646_f82c48e322_z_d.jpg",
         address: "Chinatown, Centre and Hester St Manhattan",
         location: new Location(40.7187011, -73.9994086),
+        isActive: false,
       },
       {
         id: 3,
@@ -31,6 +33,7 @@ class MarkerList extends Component {
         imgurl: "https://farm5.staticflickr.com/4407/37396722996_e18ed2225d_z_d.jpg",
         address: "Citibank, 2 Mott St, New York, NY 10013",
         location: new Location(40.7137812, -73.9985086),
+        isActive: false,
       },
       {
         id: 4,
@@ -38,6 +41,7 @@ class MarkerList extends Component {
         imgurl: "https://farm5.staticflickr.com/4418/23592508978_455f093415_z_d.jpg",
         address: "Kēkí Modern Cakes, 79 Mott St",
         location: new Location(40.716495, -73.9979633),
+        isActive: false,
       },
       {
         id: 5,
@@ -45,15 +49,74 @@ class MarkerList extends Component {
         imgurl: "https://farm5.staticflickr.com/4502/37396721486_85aa02d36e_z_d.jpg",
         address: "Chinatown, Mulberry and Canal, Manhattan",
         location: new Location(40.7171116, -73.9985879),
+        isActive: false,
+      },
+      {
+        id: 6,
+        title: "Why Not Treatment",
+        imgurl: "https://farm5.staticflickr.com/4363/23592511218_b48b67d893_z_d.jpg",
+        address: "8 Elizabeth St, New York, NY 10013, USA",
+        location: new Location(40.7156109, -73.9973547),
+        isActive: false,
+      },
+      {
+        id: 7,
+        title: "Tallmax Advertisement",
+        imgurl: "https://farm5.staticflickr.com/4378/23592510848_2d7efbb1d0_z_d.jpg",
+        address: "110 Walker St, New York, NY 10013, USA",
+        location: new Location(40.7176325, -74.0000649),
+        isActive: false,
+      },
+      {
+        id: 8,
+        title: "No Hate Sticker",
+        imgurl: "https://farm5.staticflickr.com/4489/23592508648_e427a56e1e_z_d.jpg",
+        address: "112 Walker St, New York, NY 10013, USA",
+        location: new Location(40.71755479999999, -73.99999849999999),
+        isActive: false,
+      },
+      {
+        id: 9,
+        title: "Beautiful Memory Desserts",
+        imgurl: "https://farm5.staticflickr.com/4484/37396909826_bb177a5541_z_d.jpg",
+        address: "69 Bayard St, New York, NY 10013, USA",
+        location: new Location(40.71544069999999, -73.9983151),
+        isActive: false,
+      },
+      {
+        id: 10,
+        title: "Stock Boy Hiring Sign",
+        imgurl: "https://farm5.staticflickr.com/4505/37396954576_24fdc811fe_z_d.jpg",
+        address: "216 Walker St, New York, NY 10013, USA",
+        location: new Location(40.7173419, -73.99950629999999),
+        isActive: false,
       },
     ]
+  };
+
+  toggleOpenAt = (idToOpen) => {
+    this.setState({
+      locationPics: this.state.locationPics.map((locationPic) => {
+        if(idToOpen === locationPic.id) {
+          locationPic.isActive = true;
+        } else {
+          locationPic.isActive = false;
+        }
+        return locationPic;
+      }),
+    });
   };
 
   render() {
     return (
       <div>
         {this.state.locationPics.map((locationPic) => {
-          return <LocationPicMarker key={ locationPic.id } locationPic={ locationPic } />
+          return( 
+            <LocationPicMarker 
+              key={ locationPic.id } 
+              locationPic={ locationPic }
+              toggleOpenAt={ () => this.toggleOpenAt(locationPic.id) } /> 
+          );
         })}
       </div>
     );
